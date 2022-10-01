4403dffc1eba71c19f0bc3c588ee9006e5fbdbca
Jekaterina Sedjuka <jekaterina.sedjuka@gmail.com>
2022-10-01 19:05:23 +0100

    createItemsMarkup added

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,24 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryContainer = document.querySelector(".gallery");
+const imagesMarkup = createItemsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", imagesMarkup);
+
+function createItemsMarkup(item) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+      <a class="gallery__link" href="${original.value}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>`;
+    })
+    .join('');
+}
 
 
-const createImgCards = galleryItems.map((el) => 
-  `<div class="gallery__item">
-    <a class="gallery__link" href=${el.original}>
-      <img
-        class="gallery__image"
-        src="${el.preview}"
-        data-source=${el.original}
-        alt=${el.description}
-      />
-    </a>
-  </div>`
-  ).join('');
 
-console.log(createImgCards);
-
-// console.log(galleryItems);
+// console.log(createImgCards);
 
 
 
