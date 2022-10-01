0fcccaec2795cbd3e42613be739fe9cc8ab5f2a8
Jekaterina Sedjuka <jekaterina.sedjuka@gmail.com>
2022-10-01 06:57:35 +0100

    createImgCards in progress

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,17 +3,28 @@ import { galleryItems } from './gallery-items.js';
 
 
 
+const createImgCards = galleryItems.map((el) => 
+  `<div class="gallery__item">
+    <a class="gallery__link" href=${el.original}>
+      <img
+        class="gallery__image"
+        src="${el.preview}"
+        data-source=${el.original}
+        alt=${el.description}
+      />
+    </a>
+  </div>`
+  ).join('');
 
-console.log(galleryItems);
+console.log(createImgCards);
+
+// console.log(galleryItems);
 
 
-/* <div class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
-    <img
-      class="gallery__image"
-      src="small-image.jpg"
-      data-source="large-image.jpg"
-      alt="Image description"
-    />
-  </a>
-</div> */
+
+
+// Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
+// Реализация делегирования на div.gallery и получение url большого изображения.
+// Подключение скрипта и стилей библиотеки модального окна basicLightbox. Используй CDN сервис jsdelivr и добавь в проект ссылки на минифицированные (.min) файлы библиотеки.
+// Открытие модального окна по клику на элементе галереи. Для этого ознакомься с документацией и примерами.
+// Замена значения атрибута src элемента <img> в модальном окне перед открытием. Используй готовую разметку модального окна с изображением из примеров библиотеки basicLightbox.
